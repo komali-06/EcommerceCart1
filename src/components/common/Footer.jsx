26c8e3e5200f7423bdd7fd5cd4a63fb7a3faa158
Komali <chkomali06@gmail.com>
2023-01-30 11:25:31 +0530

    'initial'

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,41 @@
+import * as Route from '@/constants/routes';
+import logo from '@/images/logo-full.png';
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+
+const Footer = () => {
+  const { pathname } = useLocation();
+
+  const visibleOnlyPath = [
+    Route.HOME,
+    Route.SHOP
+  ];
+
+  return !visibleOnlyPath.includes(pathname) ? null : (
+    <footer className="footer">
+      <div className="footer-col-1">
+        <strong>
+          <span>
+            Developed by
+            {' '}
+          </span>
+        </strong>
+      </div>
+      <div className="footer-col-2">
+        <h5>
+          &copy;&nbsp;
+          {new Date().getFullYear()}
+        </h5>
+      </div>
+      <div className="footer-col-3">
+        <strong>
+          <span>
+            Fork this project &nbsp;
+          </span>
+        </strong>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
