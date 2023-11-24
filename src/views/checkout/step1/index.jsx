3092f2759027519716bbdca8a3e65a0bc04b70f3
Rohith <varma36a@gmail.com>
2023-11-24 13:21:31 +0530

    uncomment old

--- src/views/checkout/step1/index.jsx
+++ src/views/checkout/step1/index.jsx
@@ -1,31 +1,33 @@
-import { ArrowRightOutlined, ShopOutlined } from '@ant-design/icons';
-import { BasketItem } from '@/components/basket';
-import { CHECKOUT_STEP_2 } from '@/constants/routes';
-import { displayMoney } from '@/helpers/utils';
-import { useDocumentTitle, useScrollTop } from '@/hooks';
-import PropType from 'prop-types';
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { StepTracker } from '../components';
-import withCheckout from '../hoc/withCheckout';
+import { ArrowRightOutlined, ShopOutlined } from "@ant-design/icons";
+import { BasketItem } from "@/components/basket";
+import { CHECKOUT_STEP_2 } from "@/constants/routes";
+import { displayMoney } from "@/helpers/utils";
+import { useDocumentTitle, useScrollTop } from "@/hooks";
+import PropType from "prop-types";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { StepTracker } from "../components";
+import withCheckout from "../hoc/withCheckout";
 
 const OrderSummary = ({ basket, subtotal }) => {
-  useDocumentTitle('Check Out Step 1 | Salinaka');
+  useDocumentTitle("Check Out Step 1 | Salinaka");
   useScrollTop();
   const dispatch = useDispatch();
   const history = useHistory();
-  const onClickPrevious = () => history.push('/');
+  const onClickPrevious = () => history.push("/");
   const onClickNext = () => {
-    //history.push(CHECKOUT_STEP_2)
-  }
+    history.push(CHECKOUT_STEP_2);
+  };
 
   return (
     <div className="checkout">
       <StepTracker current={1} />
       <div className="checkout-step-1">
         <h3 className="text-center">Order Summary</h3>
-        <span className="d-block text-center">Review items in your basket.</span>
+        <span className="d-block text-center">
+          Review items in your basket.
+        </span>
         <br />
         <div className="checkout-items">
           {basket.map((product) => (
@@ -47,19 +49,12 @@ const OrderSummary = ({ basket, subtotal }) => {
           <button
             className="button button-muted"
             onClick={onClickPrevious}
-            type="button"
-          >
+            type="button">
             <ShopOutlined />
-            &nbsp;
-            Continue Shopping
+            &nbsp; Continue Shopping
           </button>
-          <button
-            className="button"
-            onClick={onClickNext}
-            type="submit"
-          >
-            Next Step
-            &nbsp;
+          <button className="button" onClick={onClickNext} type="submit">
+            Next Step &nbsp;
             <ArrowRightOutlined />
           </button>
         </div>
@@ -70,7 +65,7 @@ const OrderSummary = ({ basket, subtotal }) => {
 
 OrderSummary.propTypes = {
   basket: PropType.arrayOf(PropType.object).isRequired,
-  subtotal: PropType.number.isRequired
+  subtotal: PropType.number.isRequired,
 };
 
 export default withCheckout(OrderSummary);
